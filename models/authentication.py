9e6613de48f9b5479c6004183950b73da5259d33
mhgolestan <golestan1369@gmail.com>
2025-11-28 10:21:54 +0100

    fix: Update AuthUser token initialization to use base_settings default value

--- models/authentication.py
+++ models/authentication.py
@@ -4,22 +4,14 @@ from settings import base_settings
 
 
 class AuthUser(BaseModel):
-    token: str | None = Field(default=None)
+    token: str | None = Field(default=base_settings.test_user_token)
 
-    def __init__(self, **data):
-        if 'token' not in data or data['token'] is None:
-            data['token'] = base_settings.test_user_token
-        super().__init__(**data)
+
 
 
 class Authentication(BaseModel):
     auth_token: str | None = None
-    user: AuthUser | None = None
-
-    def __init__(self, **data):
-        if 'user' not in data or data['user'] is None:
-            data['user'] = AuthUser()
-        super().__init__(**data)
+    user: AuthUser | None = AuthUser()
 
     @model_validator(mode='after')
     def validate_root(self) -> 'Authentication':
